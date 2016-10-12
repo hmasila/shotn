6fdf0cb834ef5cc9ffc93792593b515afef6f225
Hannah <hannah.masila@andela.com>
2016-10-12 11:17:51 +0300

    ch(tests): use shoulda matchers for link_spec

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,56 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  subject(:link) do
-    create(:link)
-  end
-  describe 'associations' do
-    it { should belong_to(:user) }
-  end
+  it { should belong_to(:user) }
 
-  describe '.validate_full_url' do
-    it 'must have a full_url' do
-      expect(build(:link, full_url: nil).save).to eql false
-    end
+  it { should validate_presence_of :full_url }
+  it { should_not allow_value('not a url').for(:full_url) }
+  it { should validate_presence_of :vanity_string }
+  it { should validate_uniqueness_of :vanity_string }
+  it { should validate_length_of(:vanity_string).is_at_most(6) }
 
-    it 'must have a valid full_url' do
-      expect(build(:link, full_url: 'aaaaa').save).to eql false
-    end
-  end
+  it { should have_db_column :vanity_string }
+  it { should have_db_column :title }
+  it { should have_db_column :clicks }
+  it { should have_db_column :user_id }
+  it { should have_db_index :user_id }
 
-  describe '.validate_vanity_string' do
-    it 'must have a vanity_string' do
-      expect(build(:link, vanity_string: nil).save).to eql false
-    end
-
-    it 'must have a unique vanity_string' do
-      expect(build(:link, vanity_string: link.vanity_string).save).to eql false
-    end
-
-    it 'must be less than 6 characters' do
-      expect(build(:link, vanity_string: 'my_vanity_string').save).to eql false
-    end
-  end
-
-  describe '.after_create' do
+  describe '#link_title' do
+    let(:link) { create(:link) }
     it 'must have a title' do
       expect(link.title).to_not be_nil
     end
   end
 
   describe '.most_popular' do
-    let(:link) do
-      create(:link, deleted: true)
-    end
+    let(:link) { create(:link, deleted: true) }
     it 'excludes links that are deleted' do
       expect(Link.most_popular).to_not include(link)
     end
   end
 
   describe '.most_recent' do
-    let(:link) do
-      create(:link, deleted: true)
-    end
+    let(:link) { create(:link, deleted: true) }
     it'excludes links that are deleted' do
       expect(Link.most_popular).to_not include(link)
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_many(:links) }
+  
   it { should validate_presence_of :password }
   it { should have_secure_password }
   it { should validate_confirmation_of :password }
